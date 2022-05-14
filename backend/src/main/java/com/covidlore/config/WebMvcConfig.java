75c81caf1e627be949f27a38dc1fb9c18e2d77b6
Nikita Kuprins <nikita.kuprins@gmail.com>
2022-05-14 15:02:57 +0200

    Inject port by field in WebMvcConfig

---
+++ backend/src/main/java/com/covidlore/config/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package com.covidlore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final String CLASSPATH_RESOURCE_LOCATIONS = "classpath:/public/";
+    private final Environment env;
+
+    public WebMvcConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        if (!registry.hasMappingForPattern("/webjars/**"))
+            registry.addResourceHandler("/webjars/**")
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        if (!registry.hasMappingForPattern("/**"))
+            registry.addResourceHandler("/**")
+                    .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:" + env.getProperty("server.port"));
+    }
+}
