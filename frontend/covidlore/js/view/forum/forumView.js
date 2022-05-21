f769dc20b51eb08d9a96c52ffcf8e7839baeff2f
Nikita Kuprins <nikita.kuprins@gmail.com>
2022-05-21 23:01:29 +0200

    Refactor

--- frontend/covidlore/js/view/forum/forumView.js
+++ frontend/covidlore/js/view/forum/forumView.js
@@ -3,6 +3,21 @@ class ForumView {
 
     constructor() {
         this._addNewsBody = document.querySelector('.absolute__block__body');
+        this._rotateImage();
+    }
+
+    _rotateImage() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const order = String(urlParams.get('o'));
+
+        let sortImage;
+        if (order.includes('Like'))
+            sortImage = document.querySelector('#top');
+        else if (order.includes('Date'))
+            sortImage = document.querySelector('#latest');
+
+        if (order.includes('Asc'))
+            sortImage.querySelector('.sort__icon').classList.add('rotate__sort__icon')
     }
 
     addSortButtonsListener() {
@@ -10,7 +25,13 @@ class ForumView {
     }
 
     _handleSort(e) {
-        e.target.closest('.sort__element').querySelector('.sort__icon').classList.toggle('rotate__sort__icon');
+
+        const el = e.target.closest('.sort__element');
+        const sortOption = el.id === 'top' ? 'Like' : 'Date';
+        const isAscending = !el.querySelector('.sort__icon').classList.contains('rotate__sort__icon')
+        const ascending = isAscending ? 'Asc' : 'Desc';
+
+        window.location=`/forum?o=${sortOption}${ascending}`;
     }
 
     addNewThreadListener() {
