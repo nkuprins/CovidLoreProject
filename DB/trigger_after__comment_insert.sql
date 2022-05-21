aff94d38002f05353fd9bbe1942a11e391b1a2d4
Nikita Kuprins <nikita.kuprins@gmail.com>
2022-05-22 00:00:14 +0200

    clean db

--- DB/trigger_after__comment_insert.sql
+++
@@ -1,6 +0,0 @@
-DROP TRIGGER IF EXISTS covid_lore.comment_AFTER_INSERT;
-delimiter $
-CREATE TRIGGER comment_AFTER_INSERT AFTER INSERT ON comments FOR EACH ROW 
-BEGIN
-	INSERT INTO comment_scores VALUES (NEW.comment_id, NEW.user_id, DEFAULT, DEFAULT);
-END$
