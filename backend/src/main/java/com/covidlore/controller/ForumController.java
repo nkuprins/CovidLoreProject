30d14fdc46afb723c124b6a2fc49a6955b03b70c
Nikita Kuprins <nikita.kuprins@gmail.com>
2022-08-09 16:55:28 +0300

    - Remove GetMapping method for /login and view through ViewControllerRegistry for /login - Add SessionAttribute("profileImage") in ForumController.java Besides, change way of accessing current user to AuthenticationPrincipal annotation. - Use SessionAttribute("profileImage") for other mappings in PageController.java

--- backend/src/main/java/com/covidlore/controller/ForumController.java
+++ backend/src/main/java/com/covidlore/controller/ForumController.java
@@ -5,46 +5,46 @@ import com.covidlore.entity.User;
 import com.covidlore.helper.OrderPost;
 import com.covidlore.service.PostService;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Controller
+@SessionAttributes("profileImage")
 public class ForumController {
 
     private final PostService postService;
-    private final BeanFactory beanFactory;
 
-    public ForumController(PostService postService, BeanFactory beanFactory) {
+    @Autowired
+    public ForumController(PostService postService) {
         this.postService = postService;
-        this.beanFactory = beanFactory;
     }
 
-    private User getLoggedInUser() {
-        return this.beanFactory.getBean(User.class);
+    @ModelAttribute(name = "profileImage")
+    public int userProfileImage(@AuthenticationPrincipal User loggedInUser) {
+        return loggedInUser.getProfileImage();
     }
 
     @GetMapping("/forum")
     public String showForum(@RequestParam(name = "o", required = false, defaultValue = "Default") OrderPost orderPost, Model model) {
 
-        model.addAttribute("profileImage", this.getLoggedInUser().getProfileImage());
         model.addAttribute("allPosts", postService.findAll(orderPost));
-
         return "forum";
     }
 
     @PostMapping("/saveThread")
-    public String saveThread(@RequestParam String title, @RequestParam String description) {
+    public String saveThread(@RequestParam String title, @RequestParam String description,
+                             @AuthenticationPrincipal User loggedInUser) {
 
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String date = LocalDateTime.now().format(dateFormatter);
 
-        Post newPost = new Post(this.getLoggedInUser(), date, title, description);
+        Post newPost = new Post(loggedInUser, date, title, description);
         postService.savePost(newPost);
 
         return "redirect:/forum";
--- backend/src/main/java/com/covidlore/controller/LoginController.java
+++ backend/src/main/java/com/covidlore/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.covidlore.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.security.core.Authentication;
@@ -10,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 public class LoginController {
@@ -20,13 +20,9 @@ public class LoginController {
         this.beanFactory = beanFactory;
     }
 
-    @GetMapping("/login")
-    public String showLogin() {
-        return "login";
-    }
 
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
+    public String logout(HttpServletRequest request, HttpServletResponse response, SessionStatus sessionStatus) {
 
         DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) beanFactory;
         registry.destroySingleton("loggedInUser"); //destroys the bean for reinit
@@ -35,7 +31,7 @@ public class LoginController {
         if (authentication != null)
             new SecurityContextLogoutHandler().logout(request, response, authentication);
 
-
+        sessionStatus.setComplete();
         return "redirect:/login";
     }
 }
