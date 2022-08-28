7cdaffe659760bcc6d4e9b91e94bfddc7dd1f432
Nikita Kuprins <nikita.kuprins@gmail.com>
2022-08-28 21:30:48 +0300

    .gitignore is now working

--- covid-lore-backend/post-client/src/main/java/com/example/postclient/helper/OrderPost.java
+++
@@ -1,7 +0,0 @@
-package com.example.postclient.helper;
-
-
-//@enum - represents options in which way we can order posts on forum.html
-public enum OrderPost {
-    DateAsc, DateDesc, LikeAsc, LikeDesc, Default
-}
